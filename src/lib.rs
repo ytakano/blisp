1d482bb925d5a57f4364e2ddf8dd9be1d8660246
Yuuki Takano <ytakanoster@gmail.com>
2021-02-16 17:21:16 +0900

    explain about the language in the homepage

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@
 //! Please see [blisp-repl](https://github.com/ytakano/blisp-repl) to use BLisp,
 //! or [baremetalisp](https://github.com/ytakano/baremetalisp) which is a toy OS.
 //!
+//! [Homepage](https://ytakano.github.io/blisp/) is here.
+//!
 //! ## Example
 //!
 //! ```
