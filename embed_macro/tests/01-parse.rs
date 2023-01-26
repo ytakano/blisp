1c508e27bcfd431ddd2d91b5d5929d6d7d289a5a
fumisaitou <fsaito@cy2sec.comm.eng.osaka-u.ac.jp>
2023-01-26 17:53:57 +0900

    modify test

--- embed_macro/tests/01-parse.rs
+++ embed_macro/tests/01-parse.rs
@@ -31,31 +31,43 @@
 use embed_macro::embedded;
 use num_bigint::{BigInt, ToBigInt};
 
-
-// #[sorted]
-// fn rust_function(a: BigInt, b: BigInt) -> BigInt {
-// 	a * b
-// }
-
-#[embedded]
-fn test2(
-    _z: BigInt,
-    _a: Vec<BigInt>, 
-    _b: (BigInt, BigInt),
-    _c: Option<BigInt>,
-    _d: Result<BigInt, String>,
-) -> Option<BigInt> {
-    let temp = 5.to_bigint();
-    temp
+//test_a用
+fn get_pw() -> String {
+    String::from("pass")
 }
 
 #[embedded]
-fn test_fn(a: String, _a: Option<BigInt>) -> bool {
-    match &*a {
-        "a" => true,
-        _ => false,
+fn test_a(
+    id: BigInt,
+    id_list: Vec<BigInt>,
+) -> Option<String> {
+    if id_list.into_iter().any(|x| x == id) {
+        let new_pw = get_pw();
+        Some(new_pw)
+    } else {
+        None
     }
 }
+
+// #[embedded]
+// fn test2(
+//     _z: BigInt,
+//     _a: Vec<BigInt>, 
+//     _b: (BigInt, BigInt),
+//     _c: Option<BigInt>,
+//     _d: Result<BigInt, String>,
+// ) -> Option<BigInt> {
+//     let temp = 5.to_bigint();
+//     temp
+// }
+
+// #[embedded]
+// fn test_fn(a: String, _a: Option<BigInt>) -> bool {
+//     match &*a {
+//         "a" => true,
+//         _ => false,
+//     }
+// }
 
 fn main() {
     // rust_function(2.to_bigint().unwrap(), 3.to_bigint().unwrap());
